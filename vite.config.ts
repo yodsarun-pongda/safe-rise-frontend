0bd6dc19ead8c27079d1045af6565bc91d2205f2
Yodsarun <yodsarun.cpe30@gmail.com>
2026-02-28 18:26:35 +0700

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [".yodsarun.online"],
+  },
   server: {
     host: true,
     port: 5173,
+    allowedHosts: [".yodsarun.online"],
   },
 });
